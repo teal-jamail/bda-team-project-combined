ca40e5d7dbb2bd2483996157d3ad190e8b879be0
dia sergiu <diasergiu2@gmail.com>
2026-06-14 11:54:34 +0100

    CSV modified to append mode and all_data now saves the last input

--- main.py
+++ main.py
@@ -50,16 +50,16 @@ def main():
     try:
         while True:
             speaker, phrase, time_taken_sec = record_turn(current_speaker)
-            
-            change = input("\nPress ENTER to continue or type new speaker name: ").strip()
-            current_speaker = change if change else speaker
-            
+
             all_data.append({
                 "timestamp": datetime.now().isoformat(),
                 "name": current_speaker,
                 "raw_text_vosk": phrase,
                 "time_taken_sec": time_taken_sec
             })
+
+            change = input("\nPress ENTER to continue or type new speaker name: ").strip()
+            current_speaker = change if change else speaker
 
     except KeyboardInterrupt:
         print("\nRecording stopped.")
